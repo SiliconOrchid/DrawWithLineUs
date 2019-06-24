70f0fe7cb4704f2fddbe8e604d05cc44ad6c544c
Jim <jimmcgowan.uk@gmail.com>
2019-06-24 09:44:47 +0100

    add models

---
+++ DrawWithLineUs.Console/Model/BoundingBox.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+
+namespace DrawWithLineUs.Console.Model
+{
+    public class BoundingBox
+    {
+        public Point BottomLeft { get; set; } = new Point(0, 0);
+        public Point TopRight { get; set; } = new Point(0, 0);
+
+        public int GetBoxWidth()
+        {
+            //TODO : Null handling
+            return TopRight.X - BottomLeft.X;
+        }
+
+        public int GetBoxHeight()
+        {
+            //TODO : Null handling
+            return TopRight.Y - BottomLeft.Y;
+        }
+    }
+}
---
+++ DrawWithLineUs.Console/Model/CoordinateStructure.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace DrawWithLineUs.Console.Model
+{
+    public class CoordinateStructure
+    {
+        public List<Point> ListPoints { get; set; }
+        public List<PointF> ListRescaledPoints { get; set; } //using PointF for better precision, having been scaled
+
+        public CoordinateStructure()
+        {
+            ListPoints = new List<Point>();
+            ListRescaledPoints = new List<PointF>();
+        }
+    }
+}
