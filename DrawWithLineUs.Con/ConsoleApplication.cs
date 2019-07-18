546f15c7b55ec24c2c39e8c2549e2e02b4a45f7f
Jim <jimmcgowan.uk@gmail.com>
2019-07-18 13:46:20 +0100

    Refactor DI code (so console app now supports parameter injection for DI)

---
+++ DrawWithLineUs.Con/ConsoleApplication.cs
@@ -0,0 +1,95 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using DrawWithLineUs.Con.Config;
+using DrawWithLineUs.Con.Model;
+using DrawWithLineUs.Con.Service;
+
+namespace DrawWithLineUs.Con
+{
+    public class ConsoleApplication
+    {
+        private readonly ISvgService _svgService;
+        private readonly IGeometryService _geometryService;
+        private readonly IGCodeService _gCodeService;
+        private readonly ICommunicationService _communicationService;
+
+        private static List<CoordinateStructure> _listCoordinateStructures;
+        private static List<string> _listGCodes;
+
+
+        public ConsoleApplication(
+            ISvgService svgService, 
+            IGeometryService geometryService,
+            IGCodeService gCodeService,
+            ICommunicationService communicationService
+            )
+        {
+            _svgService = svgService;
+            _geometryService = geometryService;
+            _gCodeService = gCodeService;
+            _communicationService = communicationService;
+        }
+
+        // Application starting point
+        public void Run()
+        {
+            GetCoordinatesFromSVG();
+            ApplyGeometry();
+            GenerateGCode();
+            Draw();
+        }
+
+
+        private void GenerateGCode()
+        {
+            _listGCodes = _gCodeService.GenerateGCode(_listCoordinateStructures);
+        }
+
+        private void ApplyGeometry()
+        {
+            BoundingBox sourceBoundingBox = _geometryService.DetermineSourceBounds(_listCoordinateStructures);
+
+            decimal scalingRatio = _geometryService.DetermineScalingRatio(sourceBoundingBox);
+
+            //TODO: calculate appropriate offset automatically (so as to center image in drawable area)
+            int offsetX = 800;
+            int offsetY = -500;
+
+            _geometryService.RescaleAndOffset(_listCoordinateStructures, scalingRatio, offsetX, offsetY);
+        }
+
+        private void GetCoordinatesFromSVG()
+        {
+            List<string> listPathNodes = _svgService.ExtractPaths(ProgramConfig.PathToSourceSVG);
+            _listCoordinateStructures = _svgService.ExtractCoordinates(listPathNodes);
+        }
+
+
+        private void Draw()
+        {
+            if (ProgramConfig.TestMode)
+            {
+                foreach (var gcode in _listGCodes)
+                {
+                    Console.WriteLine(gcode);
+                }
+            }
+            else
+            {
+                TcpClient client;
+                NetworkStream stream;
+                _communicationService.ConnectToLineUs(out client, out stream, ProgramConfig.LineUsIP, ProgramConfig.LineUsport);
+
+                foreach (var gcode in _listGCodes)
+                {
+                    _communicationService.Transmit(stream, gcode);
+                }
+
+                client.Close();
+            }
+        }
+    }
+}
--- DrawWithLineUs.Con/Program.cs
+++ DrawWithLineUs.Con/Program.cs
@@ -10,110 +10,31 @@ namespace DrawWithLineUs.Con
 {
     class Program
     {
-        private static IServiceProvider _serviceProvider;
-
-        private static ISvgService _svgService;
-        private static IGeometryService _geometryService;
-        private static IGCodeService _gCodeService;
-        private static ICommunicationService _communicationService;
-
-        private static List<CoordinateStructure> _listCoordinateStructures;
-        private static List<string> _listGCodes;
-
-
-
-
-
-        static void Main(string[] args)
+        static void Main()
         {
-            RegisterServices();
-            SetServices();
+            // Create service collection and configure our services
+            var services = ConfigureServices();
 
-            GetCoordinatesFromSVG();
-            ApplyGeometry();
-            GenerateGCode();
-            Draw();
+            // Generate a provider
+            var serviceProvider = services.BuildServiceProvider();
 
-            DisposeServices();
-        }
-
-        private static void Draw()
-        {
-            if (ProgramConfig.TestMode)
-            {
-                foreach (var gcode in _listGCodes)
-                {
-                    Console.WriteLine(gcode);
-                }
-            }
-            else
-            {
-                TcpClient client;
-                NetworkStream stream;
-                _communicationService.ConnectToLineUs(out client, out stream, ProgramConfig.LineUsIP, ProgramConfig.LineUsport);
-
-                foreach (var gcode in _listGCodes)
-                {
-                    _communicationService.Transmit(stream, gcode);
-                }
-
-                client.Close();
-            }
-        }
-
-        private static void GenerateGCode()
-        {
-            _listGCodes = _gCodeService.GenerateGCode(_listCoordinateStructures);
-        }
-
-        private static void ApplyGeometry()
-        {
-            BoundingBox sourceBoundingBox = _geometryService.DetermineSourceBounds(_listCoordinateStructures);
-
-            decimal scalingRatio = _geometryService.DetermineScalingRatio(sourceBoundingBox);
-
-            //TODO: calculate appropriate offset automatically (so as to center image in drawable area)
-            int offsetX = 800;
-            int offsetY = -500;
-
-            _geometryService.RescaleAndOffset(_listCoordinateStructures, scalingRatio, offsetX, offsetY);
-        }
-
-        private static void GetCoordinatesFromSVG()
-        {
-            List<string> listPathNodes = _svgService.ExtractPaths(ProgramConfig.PathToSourceSVG);
-            _listCoordinateStructures = _svgService.ExtractCoordinates(listPathNodes);
+            // Kick off our actual code
+            serviceProvider.GetService<ConsoleApplication>().Run();
         }
 
 
-        private static void RegisterServices()
+        private static IServiceCollection ConfigureServices()
         {
-            var collection = new ServiceCollection();
-            collection.AddScoped<ICommunicationService, CommunicationService>();
-            collection.AddScoped<IGCodeService, GCodeService>();
-            collection.AddScoped<IGeometryService, GeometryService>();
-            collection.AddScoped<ISvgService, SvgService>();
-            _serviceProvider = collection.BuildServiceProvider();
+            IServiceCollection services = new ServiceCollection();
+            services.AddScoped<ICommunicationService, CommunicationService>();
+            services.AddScoped<IGCodeService, GCodeService>();
+            services.AddScoped<IGeometryService, GeometryService>();
+            services.AddScoped<ISvgService, SvgService>();
+
+            services.AddScoped<ConsoleApplication>();
+            return services;
         }
 
-        private static void SetServices()
-        {
-            _svgService = _serviceProvider.GetService<ISvgService>();
-            _geometryService = _serviceProvider.GetService<IGeometryService>();
-            _gCodeService = _serviceProvider.GetService<IGCodeService>();
-            _communicationService = _serviceProvider.GetService<ICommunicationService>();
-        }
 
-        private static void DisposeServices()
-        {
-            if (_serviceProvider == null)
-            {
-                return;
-            }
-            if (_serviceProvider is IDisposable)
-            {
-                ((IDisposable)_serviceProvider).Dispose();
-            }
-        }
     }
 }
