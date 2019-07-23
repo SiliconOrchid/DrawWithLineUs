36ccc69d4c666909a3de7334f0a7b6c839ccb576
Jim <jimmcgowan.uk@gmail.com>
2019-07-23 16:34:48 +0100

    Include additional sample images (as used in article)

--- DrawWithLineUs/Config/ProgramConfig.cs
+++ DrawWithLineUs/Config/ProgramConfig.cs
@@ -2,8 +2,8 @@
 {
     public static class ProgramConfig
     {
-        public const bool TestMode = true;
-        public const string PathToSourceSVG = @"C:\parrot.svg";
+        public const bool TestMode = false;
+        public const string PathToSourceSVG = @"C:\lovebird_processed.svg";
         public const string LineUsIP = "192.168.1.212";
         public const int LineUsport = 1337;
     }
